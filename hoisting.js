ddd5f0ebb0db2a8f3efee3ac38a6029cdf3417f5
Tara Wright <wrightt2015@gmail.com>
2019-06-01 17:07:02 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -22,9 +22,9 @@ function crazy() {
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
+  var name = "Kristin";
   function sayMy() {
-    var name = "Kristin";
+    //var name = "Kristin";
     console.log(name);
 
   }
